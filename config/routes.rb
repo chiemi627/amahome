7b19b87dc6d9993204c2020f6581d80651556386
chiemi <chiemi@slis.tsukuba.ac.jpc>
2018-06-13 17:10:28 +0900

    add POST route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'purchase_items/list'
 
   get 'purchase_items/add'
+	post 'purchase_items/add'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
