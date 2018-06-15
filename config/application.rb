0f4e75427d6c087069e46ddceaedf9a1b48a004a
chiemi <chiemi@slis.tsukuba.ac.jpc>
2018-06-15 09:18:45 +0900

    add timezone

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.time_zone='Asia/Tokyo'
+
 module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
