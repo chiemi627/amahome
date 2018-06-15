22d371c9aea20e6792326d5e2f0265db24ee2b5a
chiemi <chiemi@slis.tsukuba.ac.jpc>
2018-06-15 14:55:17 +0900

    bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/add', to: 'purchase_items#add'
 	post '/agent(.:format)', to: 'purchase_items#agent'
 
+	get '/callback', to: 'purchase_items#callback'
 	post '/callback', to: 'purchase_items#callback'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
