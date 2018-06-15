8421527832d365c4632a467b15f0b65e4cddd755
chiemi <chiemi@slis.tsukuba.ac.jpc>
2018-06-15 09:34:51 +0900

    リストを表示するようにした

--- app/controllers/purchase_items_controller.rb
+++ app/controllers/purchase_items_controller.rb
@@ -4,6 +4,20 @@ class PurchaseItemsController < ApplicationController
 
   def list
     @items = Item.all
+		if @item.length == 0 then
+			speech_str = "買い物リストは空だよ"
+			
+    speech_str = @items.join("と")+"を買ってきてね"
+
+
+		msg = {
+			fulfillmentText: speech_str
+		}
+
+		respond_to do |format|
+			format.json { render json: msg }
+		end	
+
   end
 
   def add
@@ -14,7 +28,7 @@ class PurchaseItemsController < ApplicationController
     end
     new_item = Item.new(name:goods)
     new_item.save
-    speech_str = "#{goods}を追加しました"
+    speech_str = "#{goods}を追加したよ"
 
     msg = {
       fulfillmentText: speech_str
