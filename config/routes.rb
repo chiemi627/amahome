eec0d10efb2317b0e694b27a648c701d5990ec41
chiemi <chiemi@slis.tsukuba.ac.jpc>
2018-06-15 10:10:14 +0900

    bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'purchase_items/list'
+	root 'purchase_items#list'
 
+  get 'purchase_items/list'
   get 'purchase_items/add'
-	post 'purchase_items/add'
+	post 'purchase_items/agent'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
